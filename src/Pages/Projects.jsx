9e7757b4c7c19fc5ec4d01dd9b4c9c48c0200eed
Garrett Moore <robertgarrettmoore@gmail.com>
2019-04-19 08:11:03 -0700

    updated project descriptions

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -16,15 +16,15 @@ const Projects = (props) => {
         <ProjectIcons git={'https://github.com/GarrettOMoore/record-exchange'} live={'https://mighty-sands-84315.herokuapp.com/users'}/>
         <p className='project-header'>Tumblin'</p>
         <img src={tumblin} className='project-pic' width={'570px'} height={'570px'}/>
-        <p>Created using HTML5 Canvas & JavaScript - this is the evidence of a crash course in HTML5 Canvas. You are a tumbleweed - no mission, no purpose. Evading foes of no known origin, you have no choice - but to keep tumblin'...</p>
+        <p>Evidence of my crash course in HTML5 Canvas. You are a tumbleweed - no mission, no purpose. Evading foes of no known origin, you have no choice - but to keep tumblin'...</p>
         <ProjectIcons git={'https://github.com/GarrettOMoore/tumblin-game'} live={'https://garrettomoore.github.io/tumblin-game/'}/>
         <p className='project-header'>Citizenly</p>
         <img src={citizen} className='citizenly' width={'350px'} height={'620px'} />
-        <p>A collaborative project with Carlo Bruno, Hannah Erickson, and Charles Yun. Built in React, with a MongoDB backend - Citizenly is an app for those trying become more involved in US politics.</p>
+        <p>Collaborative project with three developers. Built in React, with a MongoDB backend - Citizenly is an app for those trying become more involved in US politics.</p>
         <ProjectIcons git={'https://github.com/GarrettOMoore/ga-wdi23-project-3'} live={'https://stark-meadow-11995.herokuapp.com/'}/>
         <p className='project-header'>Starbunker (24hr Hackathon)</p>
         <img src={starbunker} className='citizenly'width={'350px'} height={'620px'}/>
-        <p>Winner of General Assembly Seattle's April 2019 Hackathon. I was paired with two UX Designers (Ling Ramos, and Alana Vaccaro) and one fellow developer (Michael Cowan) to come up with an app to aid the dwindling survivors of a robot apocalypse.</p>
+        <p>Winner of General Assembly Seattle's April 2019 Hackathon. I wss paired with two UX designers to come up with an app to aid the dwindling survivors of a robot apocalypse.</p>
         <ProjectIcons git={'https://github.com/GarrettOMoore/starbunker'} live={'https://blissful-williams-7d4519.netlify.com/'}/>
         </section>
         </>
