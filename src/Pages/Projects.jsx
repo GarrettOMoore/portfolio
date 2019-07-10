33527818b926b354a36d259f6c8f550cd98f23e5
Garrett Moore <robertgarrettmoore@gmail.com>
2019-07-10 09:32:30 -0700

    added mixte

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,12 +5,17 @@ import citizen from '../Images/citizenly-1.png'
 import starbunker from '../Images/starbunker.png'
 import ProjectIcons from '../Components/ProjectIcons'
 import crema from '../Images/crema-about-mobile.png'
+import mixte from '../Images/mixte-ss.png'
 
 const Projects = (props) => {
     return(
         <>
         <p className='page-header'>Projects</p>
         <section className='projects-main'>
+        <p className='project-header'>Mixte</p>
+        <img src={mixte} className='project-pic' width={'285px'} height={'285px'} />
+        <p className='project-info'>A MERN stack app utilizing the Edamam APIs - Users can create their digital pantry, and compile miscellaneous ingredients to pull recipes from the API.</p>
+        <ProjectIcons git={'https://github.com/GarrettOMoore/edamam-fun'} live={'https://crema-gm.herokuapp.com/'}/>
         <p className='project-header'>Crema</p>
         <img src={crema} className='project-pic' width={'190px'} height={'350px'} />
         <p className='project-info'>An app for coffee enthusiasts who want not only the best brewing methods, but a little information on coffee production, where to find the best local roasters, and of course - some curated music to compliment their favorite roasts. Made with MongoDB, Express, React, and Node.</p>
